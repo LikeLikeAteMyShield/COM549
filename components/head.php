6f3f6fcbca4c23f478438e0877d067126e7a89e8
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2016-07-25 12:03:48 +0100

    add bootstrap resources and set up site header template

---
+++ components/head.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheets/bootstrap-theme.min.css">
+    <script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <?php
+        include("nav.php");
+    ?>
+</body>
+
+</html>
