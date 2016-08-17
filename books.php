b1429508e49783cdc37a1c8ee18d376b893b3c4f
adam <adam.b1993@gmail.com>
2016-08-17 15:45:51 +0100

    All books created

---
+++ books.php
@@ -0,0 +1,37 @@
+<?php 
+    include("components/head.php");
+    include("api/bookApi.php");
+
+    session_start();
+
+    if (!isset($_SESSION['name'])) {
+        header("location: login.php");
+    }
+
+    $books = getAllBooks();
+?>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>All Books</h1>
+        </div>
+    </div>
+    <div class="container">
+    <div class="row">
+
+<?php 
+    if (count($books) == 0) {
+?>
+    <p>You have no books.</p>
+<?php } ?>
+
+<?php
+    foreach($books as $book) { ?>
+        <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+            <?php echo "<img class='book-image' src='$book->image'/>"; ?>
+            <p></p>
+        </div>
+    <?php
+    }
+?>
+    </div>
+    </div>
--- components/nav.php
+++ components/nav.php
@@ -2,6 +2,7 @@
     <div class="container-fluid">
         <ul class="nav navbar-nav">
             <li><a class="navbar-brand" href="index.php">Bookmarkers</a></li>
+            <li><a href="books.php">All Books</a></li>
             <li><a href="myBooks.php">My Books</a></li>
         </ul>
     </div>
