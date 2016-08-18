20c3924046324f531b3706825530de7f75663cf2
jamiecargill <jamie@jamiecargill.com>
2016-08-18 12:46:47 +0100

    Turned out last time I didn't add the title

--- components/head.php
+++ components/head.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="stylesheets/bootstrap-theme.min.css">
     <link rel="stylesheet" href="stylesheets/main.css">
     <script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+    <title>Bookmarkers</title>
 </head>
 
 <body>
