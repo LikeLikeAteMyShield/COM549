b82f9491bcb047d02ae16b503721481e6f588860
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2016-08-18 14:44:26 +0100

    heh

--- components/nav.php
+++ components/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <ul class="nav navbar-nav">
-            <li><a class="navbar-brand" href="index.php">Bookmarkers</a></li>
+            <li><a class="navbar-brand" href="index.php">Book<strong>markers</strong></a></li>
             <li><a href="books.php">All Books</a></li>
             <li><a href="myBooks.php">My Books</a></li>
         </ul>
