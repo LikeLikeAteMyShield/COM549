7fdc230b40dce2f2bfd08dad9970bd440cbe3199
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2016-08-18 11:59:57 +0100

    colours

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 
     if (isset($_SESSION['name'])) { ?>
 
-    <div class="container">
+    <div>
         <p>Logged in as: <?php echo $_SESSION['name']; ?> <a href="logout.php">Logout</a></p>
     </div>
 
     <?php } else { ?>
 
-    <div class="container">
+    <div>
         <p>You are not logged in <a href="login.php">Login</a></p>
     </div>
 
