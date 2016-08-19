f9d15ed79251500f10bccf6aa65f2f715e4b4c48
jamiecargill <jamie@jamiecargill.com>
2016-08-19 13:15:59 +0100

    formatting a bit

--- book.php
+++ book.php
@@ -25,92 +25,90 @@
 		</div>	
 			
 		<div class="container">
-		<div class="col-sm-3">
-			
-			<?php
-					echo    "<img class='book-image' src='$book->image'/>";
-                    echo    "<p></p>";
-			?>
+			<div class="col-sm-3">
+				
+				<?php
+						echo    "<img class='book-image' src='$book->image'/>";
+	                    echo    "<p></p>";
+				?>
 
-            <?php
-            if (isset($_SESSION['name'])) {
-            ?>
-                <?php
-                if (checkIfBookIsInUserLibrary($book) == false) {
-                ?>
-                <form method="post">
-                    <button type="submit" name="add" value="add" class="btn btn-success">Add to my books</button>
-                </form>
-			<?php
-            } else {
-            ?>
-                <form method="post">
-                    <button type="submit" name="remove" value="remove" class="btn btn-danger">Remove from my books</button>
-                </form>
-			<?php 
-            }} 
-            ?>
-		</div>
-		<div class="col-sm-3" style="padding-left: 5px; padding: 5px">
+	            <?php
+	            if (isset($_SESSION['name'])) {
+	            ?>
+	                <?php
+	                if (checkIfBookIsInUserLibrary($book) == false) {
+	                ?>
+	                <form method="post">
+	                    <button type="submit" name="add" value="add" class="btn btn-success">Add to my books</button>
+	                </form>
+				<?php
+	            } else {
+	            ?>
+	                <form method="post">
+	                    <button type="submit" name="remove" value="remove" class="btn btn-danger">Remove from my books</button>
+	                </form>
+				<?php 
+	            }} 
+	            ?>
+			</div>
+			<div class="col-sm-3" style="padding-left: 5px; padding: 5px">
 
-			<table class="table">
-			<h4>
-			<tr>
-				<th>Author: </th>
-				<th><?php echo $book->author?></th>
-			</tr>
+				<table class="table">
+				<h4>
+				<tr>
+					<th>Author: </th>
+					<th><?php echo $book->author?></th>
+				</tr>
 
-			<tr>
-				<th>Genre: </th>
-				<th><?php echo $book->genre?></th>
-			</tr>
-			<tr>
-				<th>Price: </th>
-				<th><?php echo $book->price?></th>
-			</tr>
-			<tr>
-				<th>Publisher Year: </th>
-				<th><?php echo $book->publish_year?></th>
-			</tr>
-			<tr>
-				<th>Pages: </th>
-				<th><?php echo $book->pages?></th>
-			</tr>
-			</h4>
-		</table>
-		</div>
+				<tr>
+					<th>Genre: </th>
+					<th><?php echo $book->genre?></th>
+				</tr>
+				<tr>
+					<th>Price: </th>
+					<th><?php echo $book->price?></th>
+				</tr>
+				<tr>
+					<th>Publisher Year: </th>
+					<th><?php echo $book->publish_year?></th>
+				</tr>
+				<tr>
+					<th>Pages: </th>
+					<th><?php echo $book->pages?></th>
+				</tr>
+				</h4>
+			</table>
+			</div>
 
 		
 		
-		<div class="col-md-6">
-			<h3>Suggested Books</h1>
-			
-			<?php 
-				$books = filterByGenre($book->genre);
+			<div class="col-md-6">
+				<h3>Suggested Books</h1>
+				
+				<?php 
+					$books = filterByGenre($book->genre);
 
-                foreach ($books as $suggestedBook) {
-                    if (strcmp($book->title, $suggestedBook->title) != 0) {
-			?> 
+	                foreach ($books as $suggestedBook) {
+	                    if (strcmp($book->title, $suggestedBook->title) != 0) {
+				?> 
+				
+				
 			
-			
-		
-				<div class="col-sm-4"> 
-					
-					<?php 
-                    $id = $suggestedBook['id'];
-                    echo "<a href='book.php?id=$id'><img class='book-image' style='height:150px' src='$suggestedBook->image'/></a>"; 
-                    ?> 
-				<p></p> 
-				</div> 
-			
-            <?php }} ?>
-			
-	
+					<div class="col-sm-4"> 
+						
+						<?php 
+	                    $id = $suggestedBook['id'];
+	                    echo "<a href='book.php?id=$id'><img class='book-image' style='height:150px' src='$suggestedBook->image'/></a>"; 
+	                    ?> 
+					<p></p> 
+					</div> 
+				
+	            <?php }} ?>
+	        </div>
 
+	        
 			
-		</div>
-			
-		</div>	
+	</div>	
 	
 	
 <?php
